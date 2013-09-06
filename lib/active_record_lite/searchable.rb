8b56ca240ab67e0141d6fa45a837d5826a68c177
App Academy Student <student@appacademy.io>
2013-09-06 13:09:38 -0400

    Adds Searchable module.
    The Searchable module allows for searching the database for rows meeting
    certain criteria. Currently it includes a where method which finds rows whose
    fields match exact values specified by the user.
    It has been mixed into SQLObject.

--- lib/active_record_lite/searchable.rb
+++ lib/active_record_lite/searchable.rb
@@ -2,5 +2,18 @@ require_relative './db_connection'
 
 module Searchable
   def where(params)
+    keys = params.keys
+    vals = params.values
+
+    key_strings = keys.map do |key|
+      "#{key.to_s} = ?"
+    end
+    where_string = key_strings.join(" AND ")
+
+    hashes = DBConnection.execute(<<-SQL, *vals)
+    SELECT *
+    FROM #{@table_name}
+    WHERE #{where_string}
+    SQL
   end
 end
